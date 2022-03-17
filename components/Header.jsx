ab84839410f920e87e208bb3e1ec239a0f6406ed
mosesvk <kaumatule93@gmail.com>
2022-03-16 23:48:59 -0600

    customizing header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,15 +1,20 @@
 import React, { useContext, useState, useEffect } from 'react';
 import Link from 'next/link';
 
-import { getCategories } from '../services';
+
+import { getHeaders } from '../services';
 
 const Header = () => {
-  const [categories, setCategories] = useState([])
+  const [headers, setHeaders] = useState([]);
 
   useEffect(() => {
-    getCategories().then((newCategories) => setCategories(newCategories))
-  }, [])
+    getHeaders().then((newHeaders) => {
+      setHeaders(newHeaders);
+      console.log(newHeaders);
+    });
+  }, []);
 
+  const logo = '< MVK />';
 
   return (
     <div className='container mx-auto px-10 mb-8'>
@@ -17,17 +22,21 @@ const Header = () => {
         <div className='md:float-left block'>
           <Link href='/' passHref>
             <span className='cursor-pointer font-bold text-4xl text-black'>
-              MVK
+              {logo}
             </span>
           </Link>
         </div>
         <div className='hidden md:float-left md:contents'>
-          {categories.map((item) => (
-          <Link key={item.slug} href={`/category/${item.slug}`} passHref>
-            <span className='md:float-right mt-2 align-middle text-black hover:text-blue-600 active:text-blue-700 ml-4 font-semibold cursor-pointer transition duration-500 ease transform hover:-translate-y-1'>
-              {item.name}
-            </span>
-          </Link>
+          {headers.map((item) => (
+            <Link
+              key={item.node.slug}
+              href={`/category/${item.node.slug}`}
+              passHref
+            >
+              <span className='md:float-right mt-2 align-middle text-black hover:text-blue-600 active:text-blue-700 ml-4 font-semibold cursor-pointer transition duration-500 ease transform hover:-translate-y-1'>
+                {item.node.name}
+              </span>
+            </Link>
           ))}
         </div>
       </div>
--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -10,7 +10,23 @@ const PostDetails = ({post}) => {
 
   return(
     <>
-      <h1>PostDetails</h1>
+      <div className="container mx-auto px-10 mb-8">
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+          <div className="col-span-1 lg:col-span-8">
+            <PostDetail post={post} />
+            <Author author={post.author} />
+            <AdjacentPosts slug={post.slug} createdAt={post.createdAt} />
+            <CommentsForm slug={post.slug} />
+            <Comments slug={post.slug} />
+          </div>
+          <div className="col-span-1 lg:col-span-4">
+            <div className="relative lg:sticky top-8">
+              <PostWidget slug={post.slug} categories={post.categories.map((category) => category.slug)} />
+              <Categories />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
