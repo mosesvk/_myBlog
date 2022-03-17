09b2de22a21da9a4e634830761bdac981d9be584
mosesvk <kaumatule93@gmail.com>
2022-03-17 17:48:46 -0600

    customizing about page

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -5,18 +5,20 @@ import aboutImg from '../../assets/about-img.svg';
 
 const About = () => {
   return (
-    <div className='container '>
-      <div className='mx-auto px-10 mb-8 flex flex-col items-center	'>
+    <div className=''>
+      <div className='mx-auto max-w-4xl mb-8 flex flex-col items-center	'>
         <div className='w-2/5 '>
           <Image src={aboutImg} alt='about-img' />
         </div>
         <h1 className='text-3xl mb-8 font-bold pb-4 pt-10 text-center'>
           About Me
         </h1>
-        <p className='text-l px-10 text-center'>a Husband, Father and Software Engineer who wants to record my thoughts and opinions on Family, Coding, Music & Entertainment</p>
+        <p className='text-2xl px-28 pb-10 text-center'>a Husband, Father and Software Engineer who wants to record my thoughts and opinions on Family, Coding, Music & Entertainment</p>
+        <hr className='border-4 w-44 rounded border-blue-600 ' />
       </div>
-      <div className='bg-blue-100 px-20 py-25'>
-        <p className='text-l'>Married 5 years to the love of my life. We have 2 wonderful children.</p>
+      <div className='bg-gray-100 p-10'>
+        <p className='text-l px-10 text-left'>Married 5 years to the love of my life. We have 2 wonderful children.</p>
+        <p></p>
       </div>
     </div>
   );
