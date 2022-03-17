c4aaef4839bee2b0606016bf7aedb4b7de7af042
mosesvk <kaumatule93@gmail.com>
2022-03-17 14:52:43 -0600

    customizing about page

---
+++ pages/about/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Image from 'next/image';
+
+import aboutHeaderImg from '../../assets/mountain-white-1.avif';
+
+const About = () => {
+  return (
+    <div className='container mx-auto px-10 mb-8'>
+      <div className=''>
+        <h1 className='text-xl mb-8 font-bold pb-4 text-center'>
+          About Me
+        </h1>
+      </div>
+      <div className=''></div>
+    </div>
+  );
+};
+
+export default About;
