cdb8c78da0849d2ae127771127502135092f38fa
mosesvk <kaumatule93@gmail.com>
2022-03-18 09:05:30 -0600

    customizing footer

---
+++ components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import {FaLinkedinIn} from 'react-icons/fa'
+
+const Footer = () => {
+  return (
+    <div className='bg-black'>
+      <div></div>
+      <div className='flex flex-col'>
+        <FaLinkedinIn />
+      </div>
+    </div>
+  )
+}
+
+export default Footer
