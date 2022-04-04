2a19b6fe64d0a70b0209a45135ec37a12edae140
mosesvk <kaumatule93@gmail.com>
2022-04-03 22:02:16 -0600

    article details section

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react';
 import Image from 'next/image';
 
@@ -6,7 +7,7 @@ import { graphCMSImageLoader } from '../util';
 const Author = ({ author }) => (
   <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20">
     <div className="absolute left-0 right-0 -top-14">
-      <Image
+      <img
         unoptimized
         loader={graphCMSImageLoader}
         alt={author.name}
