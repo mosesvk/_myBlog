2de034ff489cd28d17087f4e920ce42063e40f22
mosesvk <kaumatule93@gmail.com>
2022-04-04 23:20:06 -0600

    working on error for the comments section

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -11,6 +11,8 @@ const Footer = () => {
           <a
             href='https://vercel.com/'
             className='underline underline-offset-2 text-sky-400 hover:text-sky-600 font-normal mx-2'
+            target='_blank'
+            rel='noreferrer'
           >
             Vercel
           </a>
@@ -31,13 +33,22 @@ const Footer = () => {
         </p>
       </div>
       <div className='flex flex-row'>
-        <a href='https://www.linkedin.com/in/mosesvk/' className='text-3xl hover:text-blue-400 duration-300'>
+        <a
+          href='https://www.linkedin.com/in/mosesvk/'
+          className='text-3xl hover:text-blue-400 duration-300'
+        >
           <FaLinkedinIn />
         </a>
-        <a href='https://www.youtube.com/channel/UChlB6LMekxCh917bjumDcCA' className='text-3xl mx-5 hover:text-blue-400 duration-300'>
+        <a
+          href='https://www.youtube.com/channel/UChlB6LMekxCh917bjumDcCA'
+          className='text-3xl mx-5 hover:text-blue-400 duration-300'
+        >
           <FaYoutube />
         </a>
-        <a href='https://www.facebook.com/profile.php?id=100004199235959' className='text-3xl hover:text-blue-400 duration-300'>
+        <a
+          href='https://www.facebook.com/profile.php?id=100004199235959'
+          className='text-3xl hover:text-blue-400 duration-300'
+        >
           <FaFacebookF />
         </a>
       </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,18 @@
 import {useEffect, useState} from 'react'
+import Router from 'next/router'
+import NProgress from 'nprogress'
 
 import {Layout} from '../components'
 
 import '../styles/globals.scss'
 import 'tailwindcss/tailwind.css'
+
+Router.events.on('routeChangeStart', (url) => {
+  console.log(`Loading: ${url}`)
+  NProgress.start()
+})
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 function MyApp({ Component, pageProps }) {
   return (
