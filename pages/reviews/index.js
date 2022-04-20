04b7c63809237fbe4c12bc6a9370f0b9116c0edd
mosesvk <kaumatule93@gmail.com>
2022-04-20 10:35:19 -0600

    working on reviews section / slugs

--- pages/reviews/index.js
+++ pages/reviews/index.js
@@ -1,23 +1,19 @@
-import Image from 'next/image'
+import Image from 'next/image';
 import { useRouter } from 'next/router';
+import { getReviews } from '../../services/index';
 
-import MainImg from '../../assets/DrawKit Larry Character Illustration/SVG/DrawKit Larry Character Illustration (1).svg'
+import MainImg from '../../assets/DrawKit Larry Character Illustration/SVG/DrawKit Larry Character Illustration (1).svg';
 
 const Reviews = () => {
+  return <div className='h-screen'></div>;
+};
 
-  return (
-    <div className='h-screen'>
-      
-      <div className='mx-auto max-w-4xl mb-8 flex flex-col items-center'>
-        <div>
-          <Image src={MainImg} alt='contructing image' />
-        </div>
-        <h1 className='text-3xl mb-8 font-bold pb-4 pt-10 text-center'>
-          This section is under Maintenance...
-        </h1>
-      </div>
-    </div>
-  )
+export default Reviews;
+
+export async function getStaticProps() {
+  const reviews = (await getReviews()) || [];
+
+  return {
+    props: { reviews },
+  };
 }
-
-export default Reviews 
