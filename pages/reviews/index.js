31a228957fcb550f1e17d5b48da7654a39233e21
mosesvk <kaumatule93@gmail.com>
2022-04-21 23:45:12 -0600

    working on reviews section / slugs

--- pages/reviews/index.js
+++ pages/reviews/index.js
@@ -1,17 +1,16 @@
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { getReviewCategories } from '../../services/index';
-import ReviewCard from '../../components/ReviewCard'
-import MainImg from '../../assets/DrawKit Larry Character Illustration/SVG/DrawKit Larry Character Illustration (1).svg';
+import ReviewCategoryCard from '../../components/ReviewCategoryCard';
+// import MainImg from '../../assets/DrawKit Larry Character Illustration/SVG/DrawKit Larry Character Illustration (1).svg';
 
 const Reviews = ({reviewCategories}) => {
+  console.log(reviewCategories)
   
   return <div className='h-screen'>
-    {reviewCategories.map((item, idx) => (
-      <div key={idx}>
-        {item.node.name}
-      </div>
-    ))}
+    {/* {reviewCategories.map((item, idx) => (
+      <ReviewCategoryCard key={idx} reviewCategory={item} />
+    ))} */}
   </div>;
 };
 
