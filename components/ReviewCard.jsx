14e3c51d718223bd0069d727a83eab17a02c54f8
mosesvk <kaumatule93@gmail.com>
2022-04-27 04:09:58 -0600

    working on reviews sections. Added anime, books, and movieshows subsections

--- components/ReviewCard.jsx
+++ components/ReviewCard.jsx
@@ -8,62 +8,24 @@ import {graphCMSImageLoader } from '../util'
 const ReviewCard = ({review}) => {
 
   return (
-    <div className='bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8'>
+    <div className=' m-4 md:w-2/6 bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8'>
       <div className='relative overflow-hidden shadow-md pb-80 mb-6'>
         <Image
-          src={review.featuredImage.url}
+          src={review.image.url}
           alt={review.title}
-          className='object-top absolute h-80 w-full object-cover  shadow-lg rounded-t-lg lg:rounded-lg'
+          className='object-top absolute h-fit w-full object-cover  shadow-lg rounded-t-lg lg:rounded-lg'
           // height={100}
           // width={100}
           layout='fill'
         />
       </div>
-      <h1 className='transition duration-700 text-center mb-8 cursor-pointer hover:text-blue-600 text-3xl font-semibold'>
-        <Link href={`/post/${review.slug}`}>{review.title}</Link>
+      <h1 className='transition duration-700 text-center mb-8 cursor-pointer hover:text-blue-600 max-h-fit text-xl font-semibold'>
+        <Link href={`/reviews/${review.slug}`}>{review.title}</Link>
       </h1>
-      <div className='block lg:flex text-center items-center justify-center mb-8 w-full'>
-        <div className='flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8 items-center'>
-          <Image
-            unoptimized
-            loader={graphCMSImageLoader}
-            alt={review.author.name}
-            height='30px'
-            width='30px'
-            className='align-middle rounded-full'
-            src={review.author.photo.url}
-          />
-          <p className='inline align-middle text-gray-700 ml-2 font-medium text-lg'>
-            {review.author.name}
-          </p>
-        </div>
-        <div className='font-medium text-gray-700'>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            className='h-6 w-6 inline mr-2 text-blue-500'
-            fill='none'
-            viewBox='0 0 24 24'
-            stroke='currentColor'
-          >
-            <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth='2'
-              d='M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z'
-            />
-          </svg>
-          <span className='align-middle'>
-            {moment(review.createdAt).format('MMM DD, YYYY')}
-          </span>
-        </div>
-      </div>
-      <p className='text-center text-lg text-gray-700 font-normal px-4 lg:px-20 mb-8'>
-        {review.exerpt}
-      </p>
       <div className='text-center'>
-        <Link href={`/post/${review.slug}`} passHref>
+        <Link href={`/reviews/${review.slug}`} passHref>
           <span className='transition duration-500 ease transform hover:-translate-y-1 inline-block bg-blue-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer'>
-            Continue Reading
+            Read Review
           </span>
         </Link>
       </div>
--- pages/reviews/books/index.js
+++ pages/reviews/books/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ReviewCard } from '../../../components';
 
 import {getBooksCategory} from '../../../services';
 import {getBookReviews} from '../../../services'
@@ -7,12 +8,17 @@ const Books = ({category, reviews}) => {
   console.log(category)
   console.log(reviews)
   return (
-    <div className='flex justify-center'>
-      <h1 className='text-3xl font-bold'>Book Reviews</h1>
-      <div>
-        
-      </div>
+    <>
+    <div className='flex flex-col justify-center containter'>
+      <h1 className='text-3xl font-bold text-center mb-8'>Book Reviews</h1>
+      <p className='text-xl italic text-center mb-8'>From Fantasy to Productivity, check out my reviews for these awesome books</p>
     </div>
+    <div className='md:flex md:flex-wrap grid-cols-2 justify-center'>
+        {reviews.map((review, idx) => (
+          <ReviewCard review={review.node} key={idx}/>
+        ))}
+      </div>
+    </>
   );
 };
 
