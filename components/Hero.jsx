767c75b6e6292b5832c8181ad28e9f79371f3264
mosesvk <kaumatule93@gmail.com>
2022-04-27 21:47:25 -0600

    folder structure added "artciles" folder and index.js

---
+++ components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='container mx-auto px-10 mb-8'>
+      <div className='border-b w-full inline-block border-blue-400 py-8'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Hero
