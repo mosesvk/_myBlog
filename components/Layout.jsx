227299a70e99487f700de90da23e39a96deab44e
mosesvk <kaumatule93@gmail.com>
2022-05-01 13:09:39 -0600

    working on layout component where the main layout is overflowing on top of the Header

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import Header from './Header'
-import Footer from './Footer'
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
 
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
   return (
     <>
       <Header />
-      {children}
+      <main>{children}</main>
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
