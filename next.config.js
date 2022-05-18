1716aab3cda80ef655cdfe1f6bb4ef0fc08d2489
mosesvk <kaumatule93@gmail.com>
2022-05-17 21:47:17 -0600

    fixing star rating image not displaying at build time

---
+++ next.config.js
@@ -0,0 +1,9 @@
+
+
+
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  // Use the CDN in production and localhost for development.
+  assetPrefix: isProd ? 'https://cdnjs.cloudflare.com' : '',
+}
