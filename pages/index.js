178b3e8af0ab2867f46f2365f89369b93b875b7b
mosesvk <kaumatule93@gmail.com>
2022-05-19 08:55:42 -0600

    Added MUI Star Rating Dynamic component

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image'
 import Layout from '@/components/Layout';
 import BannerBlock from '@/components/Banner';
 import Post from '@/components/Post';
@@ -6,6 +7,7 @@ import { getPosts } from '@/libs/getPosts';
 import { getAuthors } from '@/libs/getAuthors';
 import { getSinglePage } from '@/libs/getSinglePage';
 import { IconNewSection } from '@tabler/icons';
+import codingBg from '../public/images/background/coding.jpg'
 
 export default function Home({ authors, posts, banner, apikey }) {
   return (
@@ -21,22 +23,20 @@ export default function Home({ authors, posts, banner, apikey }) {
           </div>
         </div>
         <div className='row gy-5 gx-4 g-xl-5'>
-          {/* {posts.map((post, i) => (
-            <div key={i} className='col-lg-6'>
-              <Post post={post} authors={authors} />
-            </div>
-          ))} */}
-
-          <div className='col-12 text-center'>
-            {/* <Link href={`/blog`}>
-              <a className='btn btn-primary mt-5' aria-label='View all posts'>
-                <i className='me-2'>
-                  <IconNewSection size={16} />
-                </i>
-                View all posts
-              </a>
-            </Link> */}
+          <div className='col-lg-6 rounded'>
+            <Image src={codingBg} alt='code' layout='responsive'/>
           </div>
+
+          <Link href={`/categories/coding`} className='col-lg-6'>
+            <a className='btn btn-primary mt-5' aria-label='View all posts'>
+              <i className='me-2'>
+                <IconNewSection size={16} />
+              </i>
+              View Coding Articles
+            </a>
+          </Link>
+
+          <div className='col-12 text-center'></div>
         </div>
       </div>
     </Layout>
