9cdaab17f771b105cf4ebbe6d0257f6db69ff146
mosesvk <kaumatule93@gmail.com>
2022-05-20 12:13:53 -0600

    updating main and about page layout

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import Image from 'next/image'
+import Image from 'next/image';
 import Layout from '@/components/Layout';
 import BannerBlock from '@/components/Banner';
 import Post from '@/components/Post';
@@ -7,7 +7,7 @@ import { getPosts } from '@/libs/getPosts';
 import { getAuthors } from '@/libs/getAuthors';
 import { getSinglePage } from '@/libs/getSinglePage';
 import { IconNewSection } from '@tabler/icons';
-import codingBg from '../public/images/background/marvelPoster.jpg'
+import codingBg from '../public/images/background/marvelPoster.jpg';
 
 export default function Home({ authors, posts, banner, apikey }) {
   return (
@@ -15,26 +15,20 @@ export default function Home({ authors, posts, banner, apikey }) {
       <BannerBlock banner={banner} />
 
       <div className='container'>
-        <div className='row'>
-          <div className='col-12 text-center'>
-            {/* <h2 className='section-title'>
-              <span>Recent posts</span>
-            </h2> */}
+        <div className=''>
+          <div className='main-image-container'>
+            <Image src={codingBg} alt='code' layout='responsive' className={'main-image'} />
           </div>
-        </div>
-        <div className='row gy-5 gx-4 g-xl-5'>
-          <div className='col-lg-6 rounded'>
-            <Image src={codingBg} alt='code' layout='responsive'/>
+          <div>
+            <Link href={`/tags/marvel`} className=''>
+              <a className='btn btn-primary mt-5' aria-label='View all posts'>
+                <i className='me-2'>
+                  <IconNewSection size={16} />
+                </i>
+                View Marvel Reviews
+              </a>
+            </Link>
           </div>
-
-          <Link href={`/tags/marvel`} className='col-lg-6'>
-            <a className='btn btn-primary mt-5' aria-label='View all posts'>
-              <i className='me-2'>
-                <IconNewSection size={16} />
-              </i>
-              View Marvel Reviews
-            </a>
-          </Link>
 
           <div className='col-12 text-center'></div>
         </div>
