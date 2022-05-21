772de71f22107ba0bd2ae1380ba4b286a31ba3b6
mosesvk <kaumatule93@gmail.com>
2022-05-20 21:06:46 -0600

    working on layout component where the main layout is overflowing on top of the Header

--- pages/index.js
+++ pages/index.js
@@ -14,12 +14,12 @@ export default function Home({ authors, posts, banner, apikey }) {
     <Layout>
       <BannerBlock banner={banner} />
 
-      <div className='container'>
-        <div className=''>
+      <div className='row gy-5 gx-4 g-xl-5'>
+        <div className='col-xl-4 col-6'>
           <div className='main-image-container'>
             <Image src={codingBg} alt='code' layout='responsive' className={'main-image'} />
           </div>
-          <div>
+          <div className=''>
             <Link href={`/tags/marvel`} className=''>
               <a className='btn btn-primary mt-5' aria-label='View all posts'>
                 <i className='me-2'>
