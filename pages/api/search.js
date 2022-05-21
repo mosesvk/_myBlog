6dd15b048d8e72bff4fbc18c0d34e56594717236
mosesvk <kaumatule93@gmail.com>
2022-05-20 23:07:51 -0600

    building to vercel to check Pinterest meta tag

--- pages/api/search.js
+++ pages/api/search.js
@@ -7,7 +7,7 @@ export default (req, res) => {
 
   if (process.env.NODE_ENV === 'production') {
     // Fetch cache data
-    posts = require('../../components/cache/data').posts;
+    posts = require('../../cache/data').posts;
   } else {
     const blogDirFiles = fs.readdirSync(path.join('content/blog'));
     const blogs = blogDirFiles.filter((f) => f.includes('.md'));
