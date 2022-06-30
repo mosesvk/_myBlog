5d1f873a42fc7619d791219217173fb74838b1b1
mosesvk <kaumatule93@gmail.com>
2022-06-30 17:01:00 -0600

    update

--- components/MainContent.js
+++ components/MainContent.js
@@ -4,12 +4,15 @@ import { IconNewSection } from '@tabler/icons';
 import codingBg from '@/images/background/marvelPoster.jpg';
 import Carousel from './Carousel';
 
-const MainContent = ({recentPosts}) => {
+const MainContent = ({ recentPosts }) => {
   return (
     <div className='container'>
       <div className='lg:grid lg:grid-cols-2 my-3 '>
         <div className='text-center my-3 w-full'>
           <h2 class='text-3xl'>Recent Article Posts</h2>
+          <Carousel recentPosts={recentPosts} />
+        </div>
+        <div className='p-0 m-auto 2xl:container 2xl:mx-auto 2xl:px-0 py-3 px-10 w-full'>
           <Link href={`/tags/marvel`} className=''>
             <a className='btn btn-primary mt-5' aria-label='View all posts'>
               <i className='me-2'>
@@ -19,9 +22,6 @@ const MainContent = ({recentPosts}) => {
             </a>
           </Link>
         </div>
-        <div className='p-0 m-auto 2xl:container 2xl:mx-auto 2xl:px-0 py-3 px-10 w-full'>
-        <Carousel recentPosts={recentPosts} />
-      </div>
       </div>
     </div>
   );
