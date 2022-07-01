81e517c35db8bac12b729bfe7ca57fc56aa8433b
mosesvk <kaumatule93@gmail.com>
2022-07-01 15:50:17 -0600

    update

--- components/HeadSvg.js
+++ components/HeadSvg.js
@@ -4,7 +4,7 @@ const HeadSvg = () => {
   return (
     <svg
       className='wave text-primary'
-      width='450'
+      width='50'
       fill='none'
       version='1.0'
       xmlns='http://www.w3.org/2000/svg'
