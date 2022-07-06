7969f684a3198a9862e6f850cb4c33fab02197ec
mosesvk <kaumatule93@gmail.com>
2022-07-05 21:44:53 -0600

    update

--- components/MainContent.js
+++ components/MainContent.js
@@ -1,13 +1,12 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { IconNewSection } from '@tabler/icons';
-import codingBg from '@/images/background/marvelPoster.jpg';
 import Carousel from './Carousel';
 
 const MainContent = ({ recentPosts }) => {
   return (
     <div className='container'>
-      <div className=' my-3 '>
+      <div className='my-3'>
         <div className='text-center my-3 w-full'>
           <h2 class='text-3xl mb-4'>Recent Article Posts</h2>
           <Carousel recentPosts={recentPosts} />
